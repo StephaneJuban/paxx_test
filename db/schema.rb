377b0a1c485b486998daaf7927f23123e5a19442
Stéphane Juban <stephane.juban@gmail.com>
2015-04-01 07:34:35 +0000

    Add Author, Publisher & Book to ActiveAdmin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401070207) do
+ActiveRecord::Schema.define(version: 20150401073226) do
+
+  create_table "authors", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "books", force: true do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.integer  "author_id"
+    t.integer  "publisher_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "books", ["author_id"], name: "index_books_on_author_id"
+  add_index "books", ["publisher_id"], name: "index_books_on_publisher_id"
+
+  create_table "publishers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
